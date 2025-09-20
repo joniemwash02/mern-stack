6c3899a5bfc6b9b6a0220cdabc035858de2f3708
john chege <joniemwash2@gmail.com>
2025-09-20 20:14:32 +0300

    end of backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB connected: ${conn.connection.host}`);
+        // console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);// Exit process with failure 0 means success 1 means failure 
--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.models.js"
+import Product from "./models/product.models.js";
+import productRoutes from "./routes/product.route.js"
 dotenv.config();
 const app = express();
-app.use(express.json()) // allow us to accept json data in the body
-
+const port=process.env.PORT || 5000
+app.use(express.json()); // allow us to accept json data in the body
+app.use(express.urlencoded({ extended: true }));
 // Routes
-app.post("/api/products", async (req, res) => {
-  const product = req.body; // user will send data
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "please provide all fields" });
-  }
-  const newProduct= new Product(product)
-  try {
-    await newProduct.save();
-    res.status(201).json({success: true, data: newProduct})
-    
-  } catch (error) {
-    console.error('error in creating product', error.message)
-    res.status(500).json({success: false, message: "server error"})
-  }
-
-});
-
-app.delete("/api/products/:id", async(req, res)=>{
-    const {id}=req.params
-    console.log(id)
-})
+app.use('/api/products',productRoutes)
 
 // Connect to DB and then start server
-app.listen(5000, () => {
+app.listen(port, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000/");
+  console.log("Server started at http://localhost:/", port);
 });
